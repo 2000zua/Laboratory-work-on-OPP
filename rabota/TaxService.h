774996129eebc40c05c80889e07845e62f8247b6
Angelo Abreu Zua <Gildozua1@hotmail.com>
2023-10-31 09:30:38 +0300

    s
    s

---
+++ rabota/TaxService.h
@@ -0,0 +1,18 @@
+class TaxService {
+public:
+    static void loadJsonData(const std::string& fileName, std::vector<Owner>& owners) {
+        // Реализуйте загрузку данных из JSON-файла
+    }
+
+    static void saveJsonData(const std::string& fileName, const std::vector<Owner>& owners) {
+        // Реализуйте сохранение данных в JSON-файл
+    }
+
+    static void loadXmlData(const std::string& fileName, std::vector<Owner>& owners) {
+        // Реализуйте загрузку данных из XML-файла
+    }
+
+    static void saveXmlData(const std::string& fileName, const std::vector<Owner>& owners) {
+        // Реализуйте сохранение данных в XML-файл
+    }
+};
